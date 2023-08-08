4c236d4ada8284fbbc85282e2b357bc008b66bf8
luciaduarte <lucia.duarte@atlasteam.de>
2023-08-08 17:01:00 +0200

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <p>singularidad@gmail.com</p>
+      <p>+36 66 21 38 427</p>
+    </footer>
+  );
+};
+export default Footer;
