549473764b8110b68a43d0ebafb7f92d42b62226
luciaduarte <lucia.duarte@atlasteam.de>
2023-08-08 14:40:54 +0200

    logos & fonts

---
+++ src/assets/img/images.js
@@ -0,0 +1,8 @@
+import isologotipoC from "./isologotipoC.png";
+import logovertical from "./logovertical.jpg";
+import isologotipoN from "./isologotipoN.png";
+export default {
+  "logo-celeste": isologotipoC,
+  "logo-negro": isologotipoN,
+  "logo-vertical": logovertical,
+};
