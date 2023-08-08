49bb41ef700c6fd86b28362fe8fd5c0f48713fc7
luciaduarte <lucia.duarte@atlasteam.de>
2023-08-08 14:42:44 +0200

    added logo and updated links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
+import images from "../assets/img/images";
 
 const Navbar = () => {
   return (
     <nav className="nav-container">
       <ul className="nav-list">
         <li>
-          <Link to="/">Quiénes Somos</Link>
-        </li>
-        <li>
-          <Link to="/que-queremos">Que queremos</Link>
-        </li>
-
-        <li>
-          <Link to="/cursos-talleres">
-            Diseño especializado de Cursos/Talleres
+          <Link to="/">
+            <img src={images["logo-celeste"]} alt="Logo celeste" />
           </Link>
         </li>
         <li>
-          <Link to="/social">Proyectos social de Divulgación</Link>
+          <Link to="/quienes-somos">Quiénes Somos</Link>
         </li>
         <li>
-          <Link to="/artisticos"> Proyectos Artísticos</Link>
+          <Link to="/que-queremos">Qué queremos</Link>
         </li>
         <li>
-          <Link to="/repos"> Repositorio de IAs</Link>
+          <Link to="/cursos-talleres">Diseño de cursos y talleres</Link>
+        </li>
+        <li>
+          <Link to="/social">Proyectos sociales</Link>
+        </li>
+        <li>
+          <Link to="/artisticos"> Proyectos artísticos</Link>
+        </li>
+        <li>
+          <Link to="/repositorios"> Repositorio de IA</Link>
         </li>
         <li>
           <Link to="/recomendaciones">Recomendaciones </Link>
