6af307803cdde7523b8e0280da25a5ca4e2a9c3c
luciaduarte <lucia.duarte@atlasteam.de>
2023-08-24 14:14:16 +0200

    added pdfs download

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import "../../App.css";
 import images from "../../assets/img/images.js";
+import pdfUno from "../../assets/pdfs/1.pdf";
+import pdfDos from "../../assets/pdfs/2.pdf";
+import pdfTres from "../../assets/pdfs/3.pdf";
 
 const Inicio = () => {
   return (
     <div className="component-container">
       <div className="component-content">
-        <img
-          style={{ maxWidth: "500px", maxHeight: "auto" }}
-          src={images["logo-vertical"]}
-          alt="logo vertical"
-        ></img>
-        <p
-          style={{
-            color: "rgb(27, 161, 220)",
-            textAlign: "left",
-            verticalAlign: "bottom",
-            fontSize: "20pt",
-          }}
-        >
-          "Descubriendo <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; la pregunta"
-        </p>
+        <a href={pdfUno} target="_blank">
+          <img
+            style={{ maxWidth: "500px", maxHeight: "auto" }}
+            src={images["logo-vertical"]}
+            alt="logo vertical"
+          ></img>
+        </a>
+        <a style={{ textDecoration: "none" }} href={pdfDos} target="_blank">
+          <p
+            style={{
+              color: "rgb(27, 161, 220)",
+              textAlign: "left",
+              verticalAlign: "bottom",
+              fontSize: "20pt",
+            }}
+          >
+            "Descubriendo <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; la pregunta"
+          </p>
+        </a>
       </div>
     </div>
   );
